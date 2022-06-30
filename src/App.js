437ba471ee1b86431570d62888bb5467e1d83ee2
edmontoya1 <montoyaeduardo2015@gmail.com>
2022-06-30 17:33:27 -0500

    Init

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+
+function App() {
+  return <div className="App">App</div>;
+}
+
+export default App;
--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./App.css";
-import Home from "./pages/home/Home";
-
-const App = () => {
-  return <Home />;
-};
-
-export default App;
